e137d244be7d6909742ecf16ff8d8c8702d45481
nnWon <nwwn4627@gmail.com>
2023-12-06 13:48:49 +0900

    feat: Member 도메인 작성
    - UserRole enum클래스 작성

---
+++ src/main/java/com/ll/medium/domain/Member.java
@@ -0,0 +1,27 @@
+package com.ll.medium.domain;
+
+import com.ll.medium.domain.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    @Enumerated(value = EnumType.STRING)
+    private UserRole role;
+}
---
+++ src/main/java/com/ll/medium/domain/UserRole.java
@@ -0,0 +1,5 @@
+package com.ll.medium.domain;
+
+public enum UserRole {
+    ROLE_USER, ROLE_ADMIN
+}
