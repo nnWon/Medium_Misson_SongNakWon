55175ea33939a93298a27eeb135beba2417d0f84
nnWon <nwwn4627@gmail.com>
2023-12-11 14:10:31 +0900

    feat: CustomUserDetails 클래스 작성
    - 사용자 정보 Member 정보를 활용하기 위해서 CustomUserDetails 클래스 작성

--- src/main/java/com/ll/medium/config/security/CustomUserDetailsService.java
+++ src/main/java/com/ll/medium/config/security/CustomUserDetailsService.java
@@ -3,17 +3,11 @@ package com.ll.medium.config.security;
 import com.ll.medium.domain.Member;
 import com.ll.medium.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,16 +22,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         Member member = memberRepository.findByUsername(username) //패스워드는 스프링 시큐리티가 자동으로 처리한다.
                 .orElseThrow();
 
-        return createUser(member);
+        return new CustomUserDetails(member);
     }
 
-    private User createUser(Member member) {
-        return new User(member.getUsername(), member.getPassword(), createAuthorities(member));
-    }
-
-    private static List<GrantedAuthority> createAuthorities(Member member) {
-        return member.getRoleSet().stream()
-                .map(role -> new SimpleGrantedAuthority(role.name()))
-                .collect(Collectors.toList());
-    }
 }
